6d0d4ddc06e3945b9751014d6c70dabaf6c58276
Chimezie Edeh <princecee15@gmail.com>
2024-10-02 22:16:57 +0100

    updated grpc controller

--- apps/owners/src/controllers/owners.grpc.controller.ts
+++ apps/owners/src/controllers/owners.grpc.controller.ts
@@ -1,14 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { OwnersService } from '../services/owners.service';
 import { GrpcMethod } from '@nestjs/microservices';
-import { FindOwnerById } from '@libs/shared';
+import { GetOwnerRequest } from '@libs/shared';
 
 @Controller()
 export class OwnersGrpcController {
   constructor(private readonly ownersService: OwnersService) {}
 
   @GrpcMethod('OwnerService', 'FindById')
-  async findById(data: FindOwnerById) {
-    return this.ownersService.findOneOwner(data.ownerId);
+  async getOwner(data: GetOwnerRequest) {
+    const response = await this.ownersService.findOneOwner(data.ownerId);
+    return { owner: response.data.owner };
   }
 }
